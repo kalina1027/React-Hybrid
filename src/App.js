b347f65b06ecc34e2358d8e778334a5ccc1ee20b
kalina1027 <71938937+kalina1027@users.noreply.github.com>
2023-04-20 18:20:17 +0300

    Initial upload
    Initial upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+// import './App.css';
+import * as React from "react";
+import axios from 'axios';
+
+import CardList from "./components/CardList";
+import Form from "./components/Form";
+
+
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+  	this.setState(prevState => ({
+    	profiles: [...prevState.profiles, profileData],
+    }));
+  };
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+    	</div>
+    );
+  }	
+}
+
+export default App;
